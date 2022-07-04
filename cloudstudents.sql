cc68c43670e72261c3d9434ff3f1b84b5ccd6001
Aleksandr Logodzinskii <ekasaitlim@gmail.com>
2022-07-04 21:14:45 +0500

    Реализованы методы для задания 5: работа с файлами, задание 6 работа с директориями. Добавлен дамп базы данных

--- cloudstudents.sql
+++ cloudstudents.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3307
--- Время создания: Июн 26 2022 г., 09:54
--- Версия сервера: 10.6.5-MariaDB
--- Версия PHP: 7.4.26
+-- Хост: 127.0.0.1
+-- Время создания: Июл 04 2022 г., 18:13
+-- Версия сервера: 10.4.20-MariaDB
+-- Версия PHP: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,23 +27,42 @@ SET time_zone = "+00:00";
 -- Структура таблицы `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `users_status` varchar(255) NOT NULL,
   `age` int(11) NOT NULL,
-  `user_email` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `user_password` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `users_status` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `first_name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+  `first_name` varchar(255) NOT NULL,
+  `initial_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `age`, `user_email`, `user_password`, `users_status`, `first_name`) VALUES
-(1, 18, 'chelae1@mail.ru', '$2y$10$36ejj4A4CQ8YThTB4GJ8jOrFTP0qEWVHs/d8nkuFlVh.w5BJxul/K', 'admin', 'Aleksander');
+INSERT INTO `users` (`id`, `user_email`, `user_password`, `users_status`, `age`, `first_name`, `initial_path`) VALUES
+(2, 'ekasaitlim@gmail.com', '$2y$10$bymKNvyy6UguO8bK8Yp7IueK6IYK7sYfEBosw8O6Ez2TvobtEDfY.', 'user', 18, 'Aleksander', 'b9ca24eb5a424e34a7c8c6203cec0e42');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
